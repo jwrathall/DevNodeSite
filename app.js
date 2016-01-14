e3e1f33062c765aa16d04fcbcca1312fafc22ada
Jeff <wrathallj@gmail.com>
2016-01-14 17:01:06 -0500

    update to siteRoutes

--- app.js
+++ app.js
@@ -4,49 +4,34 @@ var app = express();
 //pull env the port obejct, if it exists use it
 var port = process.env.PORT || 5000;
 
-var siteRouter = express.Router();
+var nav = [{
+                Link: '/sites', 
+                Text:'Sites'
+            },{
+                Link: '/wiki', 
+                Text: 'Wiki'
+            },{
+                Link: '/About', 
+                Text:'About'
+            }];
+
+//view the siteRouter, notice the nav param, this is custom for the site router
+var siteRouter = require('./src/routes/siteRoutes.js')(nav);
 
 //middleware: used by express first
 //for example if express gets a request for css or js it will look here first
 app.use(express.static('public'));
+
 app.set('views', './src/views');
 
 app.set('view engine','ejs');
-
-siteRouter.route('/')
-    .get(function(req,res){
-        //this is goig to look in the views dir and for sites (line 12 app.set)
-        res.render('sites', {
-                title: 'Rendered',
-                nav:[{
-                    Link: '/sites', 
-                    Text:'Sites'
-                },{
-                    Link: '/wiki', 
-                    Text: 'Wiki'
-                },{
-                    Link: '/About', 
-                    Text:'About'
-                }]
-            }); 
-    }
-);
 
 app.use('/sites',siteRouter);
 
 app.get('/', function(req, res){
    res.render('index', {
                 title: 'Rendered',
-                nav:[{
-                    Link: '/sites', 
-                    Text:'Sites'
-                },{
-                    Link: '/wiki', 
-                    Text: 'Wiki'
-                },{
-                    Link: '/About', 
-                    Text:'About'
-                }]
+                nav: nav
             }); 
 });
 
---
+++ src/routes/siteRoutes.js
@@ -0,0 +1,23 @@
+//required for access to the router
+var express = require('express');
+
+var siteRouter = express.Router();
+
+//just a wrapper to return a router with the nav built in
+var router = function(nav){
+    siteRouter.route('/')
+        .get(function(req,res){
+            //this is goig to look in the views dir and for sites (line 12 app.set)
+            res.render('sites', {
+                    title: 'Rendered',
+                    //this is the nav that is passed into the function but defined in app.js
+                    nav: nav
+                }); 
+        }
+    );  
+    //return the updated siteRouter 
+    return siteRouter;
+};
+//export so it can be used in app.js
+//exports a function that creates a router
+module.exports = router;
